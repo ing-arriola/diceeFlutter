877c342cbc67be8648f02dca07806c28977214bc
ing-arriola <ingeniero.arriola@gmail.com>
2020-12-24 12:23:01 -0600

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blueAccent,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Center(
+            child: Text(
+              'Dicee by Jarriola',
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
+          backgroundColor: Colors.blueAccent,
         ),
         body: DicePage(),
       ),
@@ -23,6 +29,14 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+
+  random() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -31,9 +45,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = 5;
-                });
+                random();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -41,7 +53,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('right');
+                random();
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
